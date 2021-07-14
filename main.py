f4aef2067577286a6eecf82b871e9630fe271f3b
VedantK2007 <87331023+VedantK2007@users.noreply.github.com>
2021-07-14 22:38:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+# Functions
+# ---------
+# Math Definition: inputs provide outputs
+# Programming Definition: take in inputs (parameters) and return values
+# Why do we use functions?
+# Break up code into manageable chunks
+# The ability to reuse code rather than copy and pasting it.
+# Makes the code more organized.
+
+# No parameters
+
+
+# Return function
+
+
+# Python Packages
+# Python comes with libraries. Libraries/Packages in Python contain useful functions that we can use to make writing code easier for us.
+# Ex. random, turtle, example games
+# To use a library in Python we call the import statement
+# Ex. import random
+# Ex. import turtle
+
+
+
+# def multiplyNum(number1, number2):
+#   output = int(number1) * int(number2)
+#   return output
+
+# number1 = input("Enter your first number here: ")
+# number2 = input("Enter your second number here: ")
+
+# print(multiplyNum(number1, number2))
+
+
+
+# import turtle
+# wn = turtle.Screen()
+# Z = turtle.Turtle()
+# # Forward/backward units of measurement in pixels
+# Z.forward(50)
+# # for left/right movement units of measurement in angles
+# Z.left(90)
+# Z.forward(50)
+# Z.right(90)
+# Z.backward(50)
+# Z.right(90)
+# Z.forward(50)
+# Z.speed(1)
+
+import turtle
+wn = turtle.Screen()
+Z = turtle.Turtle()
+
+Z.forward(30)
+Z.left(110)
+Z.forward(50)
+Z.left(140)
+Z.forward(50)
